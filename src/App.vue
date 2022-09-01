c7163202e7975b39d7f429f0ff9c5ae1ee7e2840
Bril3d <khaledtbk@gmail.com>
2022-09-01 15:50:31 +0100

    You can search now

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-weather-primary flex flex-col min-h-screen font-Roboto">
+    <SiteNavigation />
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import SiteNavigation from './components/SiteNavigation.vue'
+import { RouterView } from "vue-router";
+</script>
+
